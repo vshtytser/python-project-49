07fda1cca073d41811e2b5c27987f386dabbd09a
Vladimir Shtytser <vshtytser@gmail.com>
2023-06-11 13:51:46 -0600

    add brain-prime game

---
+++ brain_games/games/prime_game.py
@@ -0,0 +1,12 @@
+positive_answer = "yes"
+negative_answer = "no"
+
+task = f'Answer "{positive_answer}" if the number is prime. Otherwise answer "{negative_answer}".'
+
+
+def check_if_number_prime(number):
+    divisors = []
+    for num in range(2, number):
+        if number % num == 0:
+            divisors.append(num)
+    return len(divisors) != 0
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+from brain_games.games import general_games
+from brain_games.games import prime_game
+
+
+def main():
+    general_games.welcome_user()
+    user_name = general_games.get_user_name()
+    general_games.greet_user(user_name)
+    general_games.announce_task(prime_game.task)
+
+    while general_games.number_of_rounds != 0:
+        question_content = general_games.generate_random_int()
+        general_games.ask_question(question_content)
+
+        solution = prime_game.check_if_number_prime(question_content)
+        print(f"***test_solution: {solution}")
+
+        user_answer = general_games.user_answer_input()
+
+        if prime_game.check_if_number_prime(question_content) is True:
+            solution = prime_game.positive_answer
+        else:
+            solution = prime_game.negative_answer
+
+        if general_games.check_answer(user_answer, solution) is True:
+            general_games.congratulate_user_round()
+            general_games.number_of_rounds -= 1
+        else:
+            general_games.show_correct_answer(user_answer, solution)
+            break
+
+    if general_games.number_of_rounds == 0:
+        general_games.congratulate_user_game(user_name)
+    else:
+        general_games.invite_back(user_name)
+
+
+if __name__ == '__main__':
+    main()
