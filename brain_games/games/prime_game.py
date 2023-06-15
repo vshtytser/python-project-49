8c70201e1a5facf06e2d4d5eabceedd8a8751335
Vladimir Shtytser <vshtytser@gmail.com>
2023-06-14 18:47:55 -0600

    correct function that checks if the number is prime

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -9,4 +9,4 @@ def check_if_number_prime(number):
     for num in range(2, number):
         if number % num == 0:
             divisors.append(num)
-    return len(divisors) != 0
+    return len(divisors) == 0
